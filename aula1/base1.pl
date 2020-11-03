5555c6f40e8a519cba2e581b38a06648eea60a82
Valdigleis <valdigleis@Hal9000.local>
2020-11-03 08:41:35 -0300

    Atualização dos arquivos base

--- aula1/base1.pl
+++ aula1/base1.pl
@@ -1,5 +1,5 @@
 /** 
- *   Este arquivo é a base de conhecimento 1 usada na aula 2.
+ *   Este arquivo é a base de conhecimento 1 usada na aula 1.
  *
  *   Essa base possui: 
  *       >>> 6 - fatos
@@ -12,6 +12,7 @@ feliz(abel) .              %% Fato/Functor/Termo Composto.
 feliz(kain) .              %% Fato/Functor/Termo Composto.
 ouvemusica(abel) .         %% Fato/Functor/Termo Composto.
 ouvemusica(frank).         %% Fato/Functor/Termo Composto.
+ouvemusica(ted)  .         %% Fato/Functor/Termo Composto.         
 tocaviolao(frank).         %% Fato/Functor/Termo Composto.
 
 tocaviolao(abel) :- feliz(abel), ouvemusica(abel). %% Regra/functor/Termo composto.
---
+++ aula2/base2.pl
@@ -0,0 +1,17 @@
+/** 
+ *   Este arquivo é uma base de conhecimento usada na aula 2.
+ *
+ */
+mulher(maria).
+mulher(clara).
+mulher(patricia).
+mulher(flavia).
+
+gosta(luiz, maria).
+gosta(rafael, maria).
+
+ouve(flavia, rock).
+ouve(patricia, forro).
+ouve(patricia, samba).
+
+danca(maria, forro).
