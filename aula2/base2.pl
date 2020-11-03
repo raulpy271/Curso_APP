7c8a2362eb2c1fcd5516def4bd91904974b716f4
Valdigleis <valdigleis@Hal9000.local>
2020-11-03 09:32:52 -0300

    base2 atualizada

--- aula2/base2.pl
+++ aula2/base2.pl
@@ -6,12 +6,17 @@ mulher(maria).
 mulher(clara).
 mulher(patricia).
 mulher(flavia).
+homem(luiz).
+homem(rafael).
 
 gosta(luiz, maria).
+gosta(luiz, clara).
 gosta(rafael, maria).
 
 ouve(flavia, rock).
 ouve(patricia, forro).
 ouve(patricia, samba).
+ouve(rafael, rock).
+ouve(luiz, rock).
 
 danca(maria, forro).
